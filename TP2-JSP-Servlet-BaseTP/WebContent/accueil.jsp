db5007fe0e340c9ce5f09916a95e3006189a387e
aurag <aurag.zebig@gmail.com>
2013-09-30 22:05:09 +0200

    Listing/deleting events Todo : editing and adding events

--- TP2-JSP-Servlet-BaseTP/WebContent/accueil.jsp
+++ TP2-JSP-Servlet-BaseTP/WebContent/accueil.jsp
@@ -9,19 +9,20 @@
 		<td><b>[COMMANDES]</b></td>
 	</tr>
 
-<!-- TODO Parcourir les divers événements trouves dans la servlet a l'aide de c:forEach
+<!-- TODO Parcourir les divers événements trouves dans la servlet a l'aide de c:forEach-->
+<c:forEach var="event" items="${eventList}" varStatus="status">
 		<tr>
-			<td>${evt.titre}</td>
-			<td>fmt:formatDate sur ${evt.dateDebut}</td>
-			<td>fmt:formatDate sur ${evt.dateFin}</td>
-			<td><a href="...">Détail</a><br/><a href="...">Editer</a><br/><a href="...">Supprimer</a></td>
-		</tr>
--->
+			<td>${event.titre}</td>
+			<td>fmt:formatDate sur ${event.dateDebut}</td>
+			<td>fmt:formatDate sur ${event.dateFin}</td>
+			<td><a href="show/${event.id}">Détail</a><br/><a href="edit/${event.id}">Editer</a><br/><a href="delete/${event.id}">Supprimer</a></td>
+		</tr>	
+</c:forEach>
 
 </table>
 <hr/>
 <!-- TODO ajouter le lien pour l'ajout d'un nouvel événement -->
-<center><a href="...">Ajouter un nouvel événement</a></center>
+<center><a href="add">Ajouter un nouvel événement</a></center>
 </center>
 
 <%@include file="includes/footer.jsp" %>
--- TP2-JSP-Servlet-BaseTP/src/fr/sigl/imoe/servlet/tp/servlets/ListingServlet.java
+++ TP2-JSP-Servlet-BaseTP/src/fr/sigl/imoe/servlet/tp/servlets/ListingServlet.java
@@ -1,13 +1,20 @@
 package fr.sigl.imoe.servlet.tp.servlets;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.logging.Logger;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import fr.sigl.imoe.servlet.tp.bo.Evenement;
+import fr.sigl.imoe.servlet.tp.dao.DAOFactory;
+import fr.sigl.imoe.servlet.tp.dao.EvenementDAO;
+import fr.sigl.imoe.servlet.tp.dao.hibernate.HibernateDAOFactory;
 
 /**
  * Servlet permettant d'initialiser la liste des événements
@@ -15,7 +22,7 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(
         name = "ListingServlet",
-        urlPatterns = {"/listing"}
+        urlPatterns = {"/listing", "/show/*", "/edit/*", "/delete/*", "/add"}
 )
 public class ListingServlet extends HttpServlet {
     /**
@@ -38,10 +45,81 @@ public class ListingServlet extends HttpServlet {
      * @see javax.servlet.http.HttpServlet#service(javax.servlet.http.HttpServletRequest, javax.servlet.http.HttpServletResponse)
      */
 	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Récupération de la liste des événements disponibles
+		String projectName = "/TP2-JSP-Servlet-BaseTP/";
+		String shortURI = request.getRequestURI().substring(projectName.length());
+		
+		try {
+			if (shortURI.equals("listing"))
+			{
+				DAOFactory DAOFact = DAOFactory.getDAOFactory();
+				EvenementDAO EvenementDAO = DAOFact.getEvenementDAO();
+				List<Evenement> events = EvenementDAO.getEvenements();
+				for (Evenement e : events) {
+					System.out.println(e.getTitre());
+				}
+				request.setAttribute("eventList", events);
+		
+				RequestDispatcher dispacher = getServletContext().getRequestDispatcher("/accueil.jsp");
+				dispacher.forward(request, response);
+				DAOFact.close();
+			}
+			
+			if (shortURI.startsWith("show"))
+			{
+				String id = shortURI.substring("show/".length());
+				DAOFactory DAOFact = DAOFactory.getDAOFactory();
+				EvenementDAO EvenementDAO = DAOFact.getEvenementDAO();
+				List<Evenement> events = EvenementDAO.getEvenements();
+				for (Evenement e : events) {
+					System.out.println(e.getTitre());
+				}
+				request.setAttribute("eventList", events);
+		
+				RequestDispatcher dispacher = getServletContext().getRequestDispatcher("/accueil.jsp");
+				dispacher.forward(request, response);
+				DAOFact.close();
+			}
+			
+			if (shortURI.startsWith("edit"))
+			{
+				String id = shortURI.substring("edit/".length());
+				DAOFactory DAOFact = DAOFactory.getDAOFactory();
+				EvenementDAO EvenementDAO = DAOFact.getEvenementDAO();
+				List<Evenement> events = EvenementDAO.getEvenements();
+				for (Evenement e : events) {
+					System.out.println(e.getTitre());
+				}
+				request.setAttribute("eventList", events);
+		
+				RequestDispatcher dispacher = getServletContext().getRequestDispatcher("/accueil.jsp");
+				dispacher.forward(request, response);
+				DAOFact.close();
+			}
+			
+			if (shortURI.startsWith("delete"))
+			{
+				String id = shortURI.substring("delete/".length());
+				DAOFactory DAOFact = DAOFactory.getDAOFactory();
+				EvenementDAO EvenementDAO = DAOFact.getEvenementDAO();
+				Evenement event = EvenementDAO.getEvenement(id);
+				if (!event.equals(null))
+					EvenementDAO.deleteEvenement(event);
+				DAOFact.close();
+				
+				response.sendRedirect(projectName + "listing");
+				
+			}
+			
+			if (shortURI.equals("add"))
+			{
+				response.sendRedirect(projectName + "add.jsp");
+			}
+		
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
 
-		// TODO Alimentation de la request avec les informations trouvées
-
-		// TODO Ajouter le forward vers la page JSP accueil.jsp
     }
 }
